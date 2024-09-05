b8a8d91c4fa283a12c20f86c0a6587ef5e03fe00
D940465942 <83153720+D940465942@users.noreply.github.com>
2024-09-05 23:08:14 +0800

    Add files via upload

--- inventory_management/app/database.py
+++ inventory_management/app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://admin:E5OJuKRE14iotwfsvHwJeqsjaKK1eKmO@dpg-cq0nb2aju9rs73b0500g-a.oregon-postgres.render.com/part2" #os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = "postgresql://sit722_part3_7xjc_user:c5pz0A5t53ODkjePvHQ4c5JpnxukHAKR@dpg-crcbh9dsvqrc73cht100-a.oregon-postgres.render.com/sit722_part3_7xjc" #os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
